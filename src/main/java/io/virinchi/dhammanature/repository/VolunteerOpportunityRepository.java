6895b92a8743b43df8fc62e660d3823d8972bc43
noviceGaurab <magargaurav2080@gmail.com>
2026-09-06 22:12:07 +0545

    Initial commits

--- src/main/java/io/virinchi/dhammanature/repository/VolunteerOpportunityRepository.java
+++ src/main/java/io/virinchi/dhammanature/repository/VolunteerOpportunityRepository.java
@@ -2,11 +2,14 @@ package io.virinchi.dhammanature.repository;
 
 import io.virinchi.dhammanature.model.VolunteerOpportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface VolunteerOpportunityRepository extends JpaRepository<VolunteerOpportunity, Integer> {
+    @Query("SELECT o FROM VolunteerOpportunity o JOIN FETCH o.meditationCenter " +
+            "WHERE o.opportunityDate >= :date ORDER BY o.opportunityDate ASC")
     List<VolunteerOpportunity> findByOpportunityDateGreaterThanEqualOrderByOpportunityDateAsc(java.time.LocalDate date);
     List<VolunteerOpportunity> findAllByOrderByOpportunityDateAsc();
 }
